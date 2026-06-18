34cf0770e1a741dc483ff61a9e9e5bd7e4aa60a0
royderort <royderort@gmail.com>
2026-06-17 21:21:23 -0500

    Delete AI-Review-API directory

--- AI-Review-API/database/supabase.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-from core.config import SUPABASE_URL, SUPABASE_KEY
-
-supabase = create_client(
-    SUPABASE_URL,
-    SUPABASE_KEY
-)
--- AI-Review-API/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from routes.reviews import router
-
-app = FastAPI(
-    title="AI Review Analyzer API"
-)
-
-app.include_router(router)
